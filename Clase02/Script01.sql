400cdb16de165ba67a20eb65aa695af2c75a606c
Gustavo Coronel <gcoronelc@gmail.com>
2019-02-22 11:32:58 -0500

    Clase 02

---
+++ Clase02/Script01.sql
@@ -0,0 +1,121 @@
+
+SELECT * FROM EUREKA.CLIENTE;
+
+
+-- Habilitar las salidas DBMS
+
+set serveroutput on
+
+
+-- Ejemplo 01
+
+begin
+    dbms_output.put_line('Bienvenidos al mundo de ORACLE.');
+end;
+/
+
+
+-- Ejemplo 02
+
+declare
+  v_suma   number(8);
+  v_num1   number(8);
+  v_num2   number(8);
+begin
+  -- Datos
+  v_num1 := 67;
+  v_num2 := 80;
+  -- Proceso
+  v_suma := v_num1 + v_num2;
+  -- Reporte
+  dbms_output.put_line('Número 1: ' || v_num1);
+  dbms_output.put_line('Número 2: ' || v_num2);
+  dbms_output.put_line('Suma: ' || v_suma);
+end;
+/
+
+
+-- Ejemplo 3
+
+create or replace function scott.fn_suma(
+  p_num1 in number,
+  p_num2 in number
+) return number
+is
+  v_suma number;
+begin
+  v_suma := p_num1 + p_num2;
+  return v_suma;
+end;
+/
+
+select scott.fn_suma(43,56) from dual;
+
+
+-- Ejemplo 4
+
+create or replace procedure scott.pr_suma(
+  p_num1 in  number,
+  p_num2 in  number,
+  p_suma out number
+) is
+begin
+  p_suma := p_num1 + p_num2;
+end;
+/
+
+
+DECLARE
+  P_NUM1 NUMBER;
+  P_NUM2 NUMBER;
+  P_SUMA NUMBER;
+BEGIN
+  -- Datos
+  P_NUM1 := 14;
+  P_NUM2 := 17;
+  -- Proceso
+  SCOTT.PR_SUMA(P_NUM1,P_NUM2,P_SUMA);
+  -- Reporte
+  DBMS_OUTPUT.PUT_LINE('P_SUMA = ' || P_SUMA);
+END;
+/
+
+-- SOLO EN EL SQLDBX
+BEGIN
+	SCOTT.PR_SUMA (45, 12, :p_suma$NUMBER);
+END;
+/
+
+
+-- Ejemplo 5
+
+
+create or replace procedure 
+scott.pr_lee_salario(
+  p_empno   in  number,
+  p_salario out number
+) is
+begin
+  select sal into p_salario 
+  from scott.emp 
+  where empno = p_empno;
+end;
+/
+
+declare
+  v_empno   number;
+  v_salario number;
+begin
+  -- dato
+  v_empno := 74994;
+  -- Proceso
+  scott.pr_lee_salario( v_empno, v_salario );
+  -- Reporte
+  dbms_output.put_line('Salario: ' || v_salario );
+end;
+/
+
+
+
+
+
